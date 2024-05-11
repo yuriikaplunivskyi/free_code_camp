c85cace4ca4bc7b2f0d2c81f9f1693755109416f
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-05-12 01:28:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
+const helpRegex = /please help/i;
+
 const isSpam = (msg) => false;
 
 checkMessageButton.addEventListener("click", () => {
@@ -10,5 +12,8 @@ checkMessageButton.addEventListener("click", () => {
     return;
   }
 
-
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
 });
