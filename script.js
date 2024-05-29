a99dd136a5c02e7872c4beed92acf6db0259e0d6
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-05-29 23:55:49 +0300

    feat: selectionSort

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 }
@@ -22,8 +22,6 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-      console.log(array, array[j], array[j + 1]);
-
       if (array[j] > array[j + 1]) {
         const temp = array[j];
         array[j] = array[j + 1];
@@ -34,5 +32,25 @@ const bubbleSort = (array) => {
 
   return array;
 }
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+}
+
+
 
 sortButton.addEventListener("click", sortInputArray);
