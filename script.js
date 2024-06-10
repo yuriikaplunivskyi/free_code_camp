e53749af92842b00678bcb48b14c2264c908704a
Yurii Kaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-06-11 01:45:08 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const getMean = (array) => {
+
+}
+
+const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+};
