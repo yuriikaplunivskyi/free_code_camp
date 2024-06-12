1e2f518c0fb46ae572f25e4d5c5bbb1c20d2c918
Yurii Kaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-06-12 23:34:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const getMean = (array) => {
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-}
+
 
 const calculate = () => {
-    const value = document.querySelector("#numbers").value;
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-};
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+}
