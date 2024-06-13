4d3807db53a23e5d6a350dff901ba9d8b2602826
Yurii Kaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-06-13 23:44:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
 
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
