0efbcce1fcb29121bd479d1ec28515ccb6cc3d64
Yurii Kaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-06-14 00:00:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
-
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+//it is how to find median for odd and even list
+/* const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
 console.log(oddListMedian);
+const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
+console.log(evenListMedian); */
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
